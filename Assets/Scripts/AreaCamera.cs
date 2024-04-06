c1f1164249740f23222d8a6f01018c3beb8b7dcb
aesparza84 <aesparza@colum.edu>
2024-04-06 16:57:19 -0500

    Area Camera script

---
+++ Assets/Scripts/AreaCamera.cs
@@ -0,0 +1,42 @@
+using Cinemachine;
+using UnityEngine;
+
+public class AreaCamera : MonoBehaviour
+{
+    //Area-section's camera
+    private CinemachineVirtualCamera FollowCamera;
+    
+    //Camera confiner component
+    private CinemachineConfiner2D Confiner;
+
+    //Bounding collider
+    private PolygonCollider2D BoundingShape;
+
+    private void Awake()
+    {
+        if (gameObject.GetComponentInChildren<CinemachineVirtualCamera>() != null)
+        {
+            FollowCamera = gameObject.GetComponentInChildren<CinemachineVirtualCamera>();
+            Confiner = FollowCamera.GetComponent<CinemachineConfiner2D>();
+
+            FollowCamera.gameObject.SetActive(false);
+        }
+
+        BoundingShape = GetComponentInChildren<PolygonCollider2D>();
+    }
+    private void ActivateAreaCamera()
+    {
+        FollowCamera.gameObject.SetActive(true);
+        Confiner.m_BoundingShape2D = BoundingShape;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ActivateAreaCamera();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FollowCamera.gameObject.SetActive(false);        
+    }
+}
