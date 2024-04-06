179574092694b1c15215ea250a99c28e9b87222c
Nicole Finegan <nicolefinegan@yahoo.com>
2024-04-05 22:21:15 -0500

    Player character movement update
    - Player now moves with moving platforms
    - Added level audio
    - Created a 'Player' tag to be used in the movingplatform script.
    
    - Current issue is with the player sprite renderer scaling to a larger size when on a moving platform.

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -1,7 +1,7 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//*-- Waypoint system created with assistance from 'Coding in Flow' YouTube video
 
 public class PlatformMovement : MonoBehaviour
 {
@@ -10,24 +10,65 @@ public class PlatformMovement : MonoBehaviour
 
     [SerializeField] float speed = 1f;
 
+    // Reference to the player
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find the player object by tag
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Move the platform
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, speed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < .1f)
+
+        // Check if the platform has reached the waypoint
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].transform.position) < 0.1f)
         {
+            // Move to the next waypoint
             currentWaypointIndex++;
-            if(currentWaypointIndex >= waypoints.Length)
+            if (currentWaypointIndex >= waypoints.Length)
             {
                 currentWaypointIndex = 0;
             }
-            
+        }
+    }
+
+    // LateUpdate is called after all Update functions have been called
+    void LateUpdate()
+    {
+        // Update the player's position relative to the platform
+        if (player != null && player.transform.parent == transform)
+        {
+            Vector3 offset = player.transform.position - transform.position;
+            player.transform.position = transform.position + offset;
+
+            // Reset the player's local scale
+            player.transform.localScale = Vector3.one;
+        }
+    }
+
+    // When the player collides with the platform
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Parent the player to the platform
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    // When the player leaves the platform
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Unparent the player from the platform
+            collision.transform.SetParent(null);
         }
     }
 }
