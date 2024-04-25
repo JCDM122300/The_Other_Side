b34f4cea2ed9b4e3d558d754857ff5091b14974f
samanthag168 <90341346+samanthag168@users.noreply.github.com>
2024-04-24 20:12:48 -0500

    revised level 1 and 3

--- Assets/Scripts/Interface/Parallax.cs
+++ Assets/Scripts/Interface/Parallax.cs
@@ -4,18 +4,34 @@ using UnityEngine.UI;
 
 public class Parallax : MonoBehaviour
 {
-    public GameObject bg;
-    private Camera mainCam;
-    private Vector2 screenbounds; 
+
+    Transform cam; //Main camera
+    Vector3 cameraStartPos;
+    float distance;
+
+    GameObject[] background;
+    Material[] mat;
+    float[] backSpeed;
+    float[] farthestBack;
+
+    [Range(0.01f, 0.05f)]
+    public float parallaxSpeed;
 
    
     private void Start()
     {
-        mainCam = gameObject.GetComponent<Camera>();
-        screenbounds = mainCam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCam.transform.position.z));
+        cam = Camera.main.transform;
+        cameraStartPos = cam.position;
+        int backCount = transform.childCount; 
+        mat = new Material[backCount];  
+        backSpeed = new float[backCount];   
+        background = new GameObject[backCount];
+        for (int i = 0; i < backCount; i++)
+        {
+            background[i] = transform.GetChild(i).gameObject;
+            mat[i] = background[i].GetComponent<Renderer>().material;
+
+        }
     }
-    void Update()
-    {
-       
-    }
+    
 }
